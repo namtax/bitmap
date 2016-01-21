530f9287936ea0905800d7cd792a2be13d00dd98
namtax <namtax@hotmail.com>
2016-01-21 11:54:35 +0000

    fill: allow colour blocks to be replaced with new colour

---
+++ lib/commands/fill.rb
@@ -0,0 +1,24 @@
+module Commands
+  class Fill
+    def self.run(image, input)
+      input     = input.split
+      x         = input[0].to_i
+      y         = input[1].to_i
+      new_color = input[2]
+      color     = image.table[-y][x-1]
+
+      color_pixel(image, x-1, -y, color, new_color)
+    end
+
+    def self.color_pixel(image, x, y, color, new_color)
+      return if (image.table[y][x] != color || image.table[y][x] == new_color )
+
+      image.table[y][x] = new_color
+
+      color_pixel(image, x + 1, y, color, new_color)
+      color_pixel(image, x - 1, y, color, new_color)
+      color_pixel(image, x, y + 1, color, new_color)
+      color_pixel(image, x, y - 1, color, new_color)
+    end
+  end
+end
